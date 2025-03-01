9ad25de9abcbd7305d5ee9702093a60e1a1fda16
IT Service <itservice@WynnSoft-Solution.com>
2025-03-01 09:39:26 +0700

    aos1

--- resources/views/pages/room/room.blade.php
+++ resources/views/pages/room/room.blade.php
@@ -13,14 +13,14 @@
     <section class=" w-full h-auto relative z-50">
         <div
             class="absolute inset-0 z-50  flex flex-col justify-center items-center gap-y-4  max-sm:gap-y-1 max-w-[1536px] m-auto">
-            <p class="text-white text-6xl max-xl:text-3xl  font-bold text-center" data-aos="zoom-in" data-aos-duration="500">
+            <div class="text-white text-6xl max-xl:text-3xl  font-bold text-center animate__animated animate__fadeInUp">
             <div class="flex flex-col sm:gap-y-8">
-                <p class="font-kanit xl:text-8xl ss:text-6xl text-4xl uppercase" data-aos="zoom-in" data-aos-duration="500"
+                <p class="font-kanit xl:text-8xl ss:text-6xl text-4xl uppercase"
                     style="text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.7);">
                     room
                 </p>
             </div>
-            </p>
+            </div>
         </div>
 
         <div class="relative w-full h-[600px] max-2xl:h-[500px] max-lg:h-[350px] max-md:h-[250px]">
